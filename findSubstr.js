6ddb615409825b7537bbf97e73ae3c0dafc14a76
fenya <dim74.09@mail.ru>
2022-10-31 15:55:37 +0500

    rabin-karp added

--- findSubstr.js
+++ findSubstr.js
@@ -21,32 +21,54 @@ String.prototype.getHash = function (mode) {
             }
             return  hashSum;
         case "Rabin–Karp algorithm":
-
-            break;
+            for(let i = 0; i < this.length; i++) {
+                hashSum += this.charCodeAt(i)*2**(this.length-i-1);
+            }
+            return hashSum;
     }
-    return hashSum;
+    return null;
 }
 
 String.prototype.findSubstrUsingBruteForce = function (substr) {
     if(substr === "") return null;
-    substr = String(substr);
+    //substr = String(substr);
     let matches = [];
 
     for(let i = 0; i < this.length-substr.length+1; i++) {
-        if(this[i] === substr[0]) {
-            let match = true;
-            for(let j = 1; j < substr.length; j++) {
-                if(this[i+j] !== substr[j]) {
-                    match = false;
-                    break;
-                }
+        let match = true;
+        for(let j = 0; j < substr.length; j++) {
+            if(this[i+j] !== substr[j]) {
+                match = false;
+                break;
             }
-            if(match) matches.push(i);
+        }
+        if (match) matches.push(i);
+    }
+    /*if(matches.length > 0)*/ return matches;
+    //else return null;
+};
+
+
+function cmpChar(str,i,substr,array){
+    let f = true
+    let t = str.slice(i, i + substr.length)
+    for (let j = 0; j < substr.length; j++) {
+        if (substr[j] !== t[j]) {
+            f = false
+            break
         }
     }
-    if(matches.length > 0) return matches;
-    else return null;
-};
+    if (f) array.push(i)
+}
+
+String.prototype.BruteForce = function(substr) {
+    if (substr==="") return null
+    let indexBF = []
+    for (let i = 0; i < this.length - substr.length+1; i++) {
+        cmpChar(this,i,substr,indexBF)
+    }
+    return indexBF
+}
 
 String.prototype.findSubstrUsingHash = function (substr, way) {
 // "ways" arg watch in "ФУНДАМЕНТАЛЬНЫЕ ЗАДАЧИ ИНФОРМАТИКИ. СКРИПТЫ" by СОЛОДУШКИН Святослав Игоревич (page 36).
@@ -137,39 +159,75 @@ String.prototype.findSubstrUsingHash = function (substr, way) {
             if (matches.length > 0) return matches;
             else return null;
         }
+        case 4: { // rabin-karp
+            substrHash = substr.getHash("Rabin–Karp algorithm");
+            let fragmentHash = 0;
+            for (let i = 0; i < substr.length; i++)
+                fragmentHash += this.charCodeAt(i)*2**(substr.length-i-1); // edition
+            if (fragmentHash === substrHash) { // first entrance check individually.
+                let match = true;
+                for (let i = 0; i < substr.length; i++) {
+                    if (this[i] !== substr[i]) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) matches.push(0);
+            }
+            for (let i = 1; i < this.length - substr.length + 1; i++) {
+                fragmentHash = (fragmentHash - this.charCodeAt(i-1)*2**(substr.length-1))*2 + this.charCodeAt(i + substr.length - 1); // edition
+                if (fragmentHash === substrHash) {
+                    let match = true;
+                    for (let j = 0; j < substr.length; j++) {
+                        if (this[i + j] !== substr[j]) {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (match) matches.push(i);
+                }
+            }
+            if (matches.length > 0) return matches;
+            else return null;
+        }
     }
     return null;
 }
 
 
-let str;
+// let str = "ABCDEFG";
+// let substr = "ABCDEFG";
+
 fs = require('fs');
-//str = fs.readFileSync("./testFiles/1000000a.txt").toString();
+let str = fs.readFileSync("./testFiles/1000000a.txt").toString();
+substr = "a".repeat(100);
 
-let substr = "";
-substr = substr.repeat(100) + "b";
-// console.log(substr);
-str += fs.readFileSync("./testFiles/war and peace(1 tome).txt").toString();
- str += fs.readFileSync("./testFiles/war and peace(2 tome).txt").toString();
- str += fs.readFileSync("./testFiles/war and peace(3 tome).txt").toString();
- str += fs.readFileSync("./testFiles/war and peace(4 tome).txt").toString();
 
-// console.time("Андрей");
-// console.log(str.findSubstrUsingBruteForce("фортификация которых производилась"));
-// console.timeEnd("Андрей");
-//
-// console.time("князь Андрей");
-// console.log(str.findSubstrUsingBruteForce(" которых производилась"));
-// console.timeEnd("князь Андрей");
-//
-// console.time("князь Андрей Болконский");
-// console.log(str.findSubstrUsingBruteForce("князь Андрей Болконский").length);
-// console.timeEnd("князь Андрей Болконский");
+// //WAR AND PEACE...
+// fs = require('fs');
+// let str = "";
+// str += fs.readFileSync("./testFiles/war and peace(1 tome).txt").toString();
+// str += fs.readFileSync("./testFiles/war and peace(2 tome).txt").toString();
+// str += fs.readFileSync("./testFiles/war and peace(3 tome).txt").toString();
+// str += fs.readFileSync("./testFiles/war and peace(4 tome).txt").toString();
+// let substr = "князь Андрей"
+
+console.time("sqr");
+str.findSubstrUsingHash(substr, 3);
+console.timeEnd("sqr");
+
+console.time("sum");
+str.findSubstrUsingHash(substr, 2);
+console.timeEnd("sum");
+
+console.time("sum(not rec)");
+str.findSubstrUsingHash(substr, 1);
+console.timeEnd("sum(not rec)");
 
 console.time("bf");
-console.log(str.findSubstrUsingBruteForce(substr));
+str.findSubstrUsingBruteForce(substr);
 console.timeEnd("bf");
 
-console.time("hash");
-console.log(str.findSubstrUsingHash(substr, 2));
-console.timeEnd("hash");
+console.time("rk");
+str.findSubstrUsingHash(substr, 4);
+console.timeEnd("rk");
